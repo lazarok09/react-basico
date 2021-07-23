7a3f45122c149341c291d125f5c3dcc708daf09b
lazarok09 <lazarok09@gmail.com>
2021-07-23 11:40:53 -0300

    style: estiliza componente NoPostsCard

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,7 +1,7 @@
 export const PostCard = (props) => {
   const { ...post } = props;
   return (
-    <div key={post.id}>
+    <div id="post-card" key={post.id}>
       <img src={post.photo} alt={post.title} />
       <h1>{post.title}</h1>
       <p>{post.body}</p>
