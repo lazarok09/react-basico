b7b296e0b4d36504ad353658ef13e25b0986d697
lazarok09 <lazarok09@gmail.com>
2021-07-28 10:58:21 -0300

    doc(noPostsCard) descreve o teste

--- src/components/NoPostsCard/NoPostsCard.spec.jsx
+++ src/components/NoPostsCard/NoPostsCard.spec.jsx
@@ -5,6 +5,7 @@ import { NoPostsCard } from "./index.jsx";
 describe("<NoPostsCard />", () => {
   it("should render the text properly", () => {
     render(<NoPostsCard />);
+    // testa se o texto confere com o renderizado
     expect(
       screen.getByText(/Opa, parece que não encontramos /i)
     ).toBeInTheDocument();
